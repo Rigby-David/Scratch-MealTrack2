78b9ba50052c021356f0a40f5cd743a33a7dd2f3
David <Rigby335@hotmail.com>
2022-05-03 17:49:06 -0700

    got ids, started form listener

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // import functions and grab DOM elements
+const form = document.getElementById('add-ingredient');
+const ingredientList = document.getElementById('ingredient-list');
+const mealList = document.getElementById('meal-list');
+
+const remove = document.getElementById('remove');
+
+const mealName = document.getElementById('meal-name');
+const save = document.getElementById('save-meal');
 
 // let state
+let ingredients = [];
+let meals = [];
 
-// set event listeners 
+// set event listeners
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
